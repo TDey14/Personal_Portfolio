4cf313372118a5a404566aaaf3bbe328a6084992
T_Dey <135143008+TDey14@users.noreply.github.com>
2024-10-28 21:35:10 +0530

    Add files via upload

---
+++ personal.js
@@ -0,0 +1,13 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+
+    event.preventDefault();
+
+    alert('Thank you for reaching out!');
+
+    this.reset();
+
+});
+
+ 
+
+ 
